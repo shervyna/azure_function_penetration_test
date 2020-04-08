18f65c2db0e39ab2825e50d6211efaa2e6b83c8e
Shervyna Ruan <HsuanWei.Ruan@microsoft.com>
2020-04-08 19:42:22 +0800

    docker

--- user/__init__.py
+++ user/__init__.py
@@ -6,27 +6,32 @@ import psycopg2
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.route_params.get('name')
+    name = req.params.get('name')
 
     if name:
         try:
-            conn = psycopg2.connect(database = "penTest", user = "postgres", password = "postgres", host = "localhost", port = "5432")
+            conn = psycopg2.connect(
+                database = "penTest",
+                user = "postgres", 
+                password = "postgres",
+                host = "host.docker.internal",
+                port = "5432")
             with conn.cursor() as cursor:
                 cursor.execute("SELECT * FROM users WHERE name = '%s' " % name)
                 result = cursor.fetchall()
             conn.commit()
             conn.close()
             if result:
-                return func.HttpResponse(f"{result}")
+                return func.HttpResponse(f"{result}", mimetype='application/json')
             else:
-                return func.HttpResponse("no data")
+                return func.HttpResponse("no data",mimetype='application/json')
         except Exception as e:
             print(f"error: {e}")
-            return func.HttpResponse(f"{e}", status_code=500)
+            return func.HttpResponse(f"{e}", status_code=500,mimetype='application/json')
 
        
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+             status_code=200,mimetype='application/json'
         )
